fd609c949b5bbb0a677c97d2b5356fee7e2e604e
julietk <taziukyulia@gmail.com>
2023-11-25 22:12:38 +0200

    lab1/step1

--- lab1/StringCalculator/src/main/java/org/calculator/StringCalculator.java
+++ lab1/StringCalculator/src/main/java/org/calculator/StringCalculator.java
@@ -1,4 +1,23 @@
 package org.calculator;
 
 public class StringCalculator {
+    public int add(String numbers) {
+
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+
+        String[] numbers_array = numbers.split(",");
+
+        if (numbers_array.length > 2) {
+            throw new IllegalArgumentException("Only two numbers are allowed.");
+        }
+
+        int sum = 0;
+        for (String number : numbers_array) {
+            int num = Integer.parseInt(number);
+            sum += num;
+        }
+        return sum;
+    }
 }
--- lab1/StringCalculator/src/test/java/org/calculator/StringCalculatorTest.java
+++ lab1/StringCalculator/src/test/java/org/calculator/StringCalculatorTest.java
@@ -1,4 +1,29 @@
 package org.calculator;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 public class StringCalculatorTest {
+    StringCalculator calculator = new StringCalculator();
+
+    @Test
+    void EmptyStringsSumIsZero() {
+        assertEquals(0, calculator.add(""));
+    }
+
+    @Test
+    void NumberIsEqualToItself() {
+        assertEquals(1, calculator.add("1"));
+    }
+
+    @Test
+    void SumOfTwoComaSeparatedNumbers() {
+        assertEquals(3, calculator.add("1,2"));
+    }
+
+    @Test
+    void SumOfMoreThanTwoNumbersIsNotAllowed() {
+        assertThrows(IllegalArgumentException.class,
+                () -> calculator.add("1,2,3"));
+    }
 }
