8c8364d258238b664bccadfbb20816ba590562ac
julietk <taziukyulia@gmail.com>
2023-11-26 01:48:55 +0200

    lab2/final

---
+++ lab2/src/main/java/org/matrix/Dimension.java
@@ -0,0 +1,12 @@
+package org.matrix;
+
+public class Dimension {
+    public int rowsNum;
+    public int columnsNum;
+
+    public Dimension(int rowsNum, int columnsN) {
+        this.rowsNum = rowsNum;
+        this.columnsNum = columnsN;
+    }
+}
+
